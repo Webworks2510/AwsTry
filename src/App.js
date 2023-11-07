c3bf912e524109b8c97be591c04c56677dcfe1a2
sj210033 <sj2100333@gmail.com>
2023-11-07 11:47:53 +0530

    uncompleted

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    axios.get('https://backend-api-production-a9b6.up.railway.app/getuser')
+    .then(res=>setData(res.data))
+  },[]);
+  return (
+    <div className="App">
+      <>
+      <table className='table container-md'>
+        <thead>
+          <th>Username</th>
+          <th>Email</th>
+        </thead>
+        <tbody>
+          {data.map(m=>
+            <tr key={m.customerID}>
+              <td>{m.customerID}</td>
+              <td>{m.mail}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      </>
+    </div>
+  );
+}
+
+export default App;
